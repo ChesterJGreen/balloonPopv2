dc82d053ae3e18b08039a7f3a6c4c47a503622f2
cj <seejarom@gmail.com>
2022-08-29 17:25:48 -0600

    abstracting draw

--- main.js
+++ main.js
@@ -1,24 +1,50 @@
+let startButton = document.getElementById('start-button')
+let inflateButton = document.getElementById('inflate-button')
+
 let clickCount = 0
 let height = 120
 let width = 100
 let inflationRate = 20
 let maxSize = 300
+let popCount = 0
+
+function startGame() {
+
+  startButton.setAttribute("disabled", "true")
+  inflateButton.removeAttribute("disabled")
+
+setTimeout(()=> { 
+  console.log("it's been 3 seconds")
+
+  inflateButton.setAttribute("disabled", "true")
+  startButton.removeAttribute("disabled")
+  height = 0
+  width = 0
+  
+}, 3000)
+}
 
 function inflate(){
   clickCount ++
-  let balloonElement = document.getElementById("balloon")
   height += inflationRate
   width += inflationRate
   if (height >= maxSize){
     console.log("pop the balloon")
+    popCount++
     height = 0
     width = 0
   }
+  draw()  
+}
+
+function draw() {
+  let balloonElement = document.getElementById("balloon")
+  let clickCountElement = document.getElementById("click-count")
+  let popCountElement = document.getElementById('pop-count')
+  
   balloonElement.style.height = height + "px"
   balloonElement.style.width = width + "px"
-
-
-  let clickCountElement = document.getElementById("click-count")
+  
   clickCountElement.innerText = clickCount.toString()
-
+  popCountElement.innerText = popCount.toString()
 }
